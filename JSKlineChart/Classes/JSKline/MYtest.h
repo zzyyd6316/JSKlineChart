8dc9c16761931863fd66d27b424c3590f46148c8
zhangzeyang <zhangzeyang@xcloud.cc>
2018-08-02 12:49:45 +0800

    0.1.7创建mytest

---
+++ JSKlineChart/Classes/JSKline/MYtest.h
@@ -0,0 +1,13 @@
+//
+//  MYtest.h
+//  MyKlineDemo
+//
+//  Created by zzy on 2018/8/2.
+//  Copyright © 2018年 zzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYtest : NSObject
+
+@end
---
+++ JSKlineChart/Classes/JSKline/MYtest.m
@@ -0,0 +1,13 @@
+//
+//  MYtest.m
+//  MyKlineDemo
+//
+//  Created by zzy on 2018/8/2.
+//  Copyright © 2018年 zzy. All rights reserved.
+//
+
+#import "MYtest.h"
+
+@implementation MYtest
+
+@end
